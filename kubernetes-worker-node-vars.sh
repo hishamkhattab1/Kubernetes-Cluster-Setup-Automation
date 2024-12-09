bcbdaf7ecc390bbb4b0f5530939c20997b2b1dc9
hishamkhattab1 <89085957+hishamkhattab1@users.noreply.github.com>
2024-12-09 13:15:49 +0200

    Create kubernetes-worker-node-vars.sh
    Variables needed to start the setup of the worker nodes

---
+++ kubernetes-worker-node-vars.sh
@@ -0,0 +1,5 @@
+# Get those data by typing the following command in the master node: "kubeadm token create --print-join-command" 
+# Replace the following lines with the data you got and you are good to go!!
+MASTER_IP=192.168.180.192:6443
+TOKEN=5peckq.99unltrhowut21dw
+DISCOVERY_TOKEN_CA_CERT_HASH=sha256:c5911f79c40f9a59051bc810def92a0f57c5a8b1e9555c97f71f5819aa4fbf3
